9e6db84345da77aeda5587e02ec5f9e5c50c7f7f
Clickadelic <toby.hopp@gmail.com>
2024-02-10 12:59:53 +0100

    cleanup

--- src/actions.js
+++ src/actions.js
@@ -1,22 +1,6 @@
-import { Collapse, initTE } from "tw-elements";
-
-initTE({ Collapse });
-
-document.addEventListener("DOMContentLoaded", function () {
-	var toggleButton = document.getElementById("sidebar-toggle");
-
-	toggleButton.addEventListener("click", function () {
-		this.classList.toggle("active");
-		// Additional state change actions can be performed here
-		alert("toggled");
+document.addEventListener("DOMContentLoaded",  () => {
+	const toggleButton = document.getElementById("btn-sidebar-toggle");
+	toggleButton.addEventListener("click", () => {
+		alert("Toggle");
 	});
-});
-
-let isToggled = false;
-
-toggleButton.addEventListener("click", () => {
-	isToggled = !isToggled;
-	this.classList.toggle("active");
-	// You may also want to trigger other actions based on the value of isToggled
-	alert("toggled");
 });
