61f786815fa8ebdeceef7a750f7cd363d8c2c231
Clickadelic <toby.hopp@gmail.com>
2024-02-19 15:51:02 +0100

    details

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,6 +1,30 @@
-document.addEventListener("DOMContentLoaded",  () => {
-	const toggleButton = document.getElementById("btn-sidebar-toggle");
-	toggleButton.addEventListener("click", () => {
-		alert("Toggle");
-	});
+class SidebarToggle {
+	constructor() {
+		this.addToggleListener();
+	}
+
+	addToggleListener() {
+		document.getElementById("btn-sidebar-toggle").addEventListener("click", () => {
+			this.toggleSidebar();
+		});
+	}
+
+	toggleSidebar() {
+		const sidebar = document.getElementById("main-sidebar");
+		const logo = document.getElementById("tailwind-dashboard");
+		console.log(this.texts);
+		if (sidebar.classList.contains("w-64")) {
+			logo.classList.add("hidden");
+			sidebar.classList.add("w-16");
+			sidebar.classList.remove("w-64");
+		} else if (sidebar.classList.contains("w-16")) {
+			logo.classList.remove("hidden");
+			sidebar.classList.add("w-64");
+			sidebar.classList.remove("w-16");
+		}
+	}
+}
+
+window.addEventListener("load", () => {
+	const sidebarToggle = new SidebarToggle();
 });
