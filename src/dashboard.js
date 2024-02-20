40da06111a3cca09b4280c228fdf7bba8032faa8
Clickadelic <toby.hopp@gmail.com>
2024-02-21 00:51:14 +0100

    details with bugs

--- src/dashboard.js
+++ src/dashboard.js
@@ -13,7 +13,9 @@ class SidebarToggle {
 		const header = document.getElementById("header");
 		const logotext = document.getElementById("tailwind-dashboard");
 		const sidebar = document.getElementById("main-sidebar");
+		const avatar = document.getElementById("avatar-box");
 		const dashboardarea = document.getElementById("dashboard-area");
+		const items = document.getElementsByClassName("text-toggle");
 		if (sidebar.classList.contains("w-64")) {
 			logotext.classList.add("hidden");
 			header.classList.remove("md:ml-64");
@@ -22,6 +24,11 @@ class SidebarToggle {
 			dashboardarea.classList.add("md:pl-24");
 			sidebar.classList.add("w-16");
 			sidebar.classList.remove("w-64");
+			avatar.classList.add("w-16");
+			avatar.classList.remove("w-64");
+			for (let item of items) {
+				item.classList.add("hidden");
+			}
 		} else if (sidebar.classList.contains("w-16")) {
 			logotext.classList.remove("hidden");
 			header.classList.remove("md:ml-16");
@@ -30,6 +37,11 @@ class SidebarToggle {
 			dashboardarea.classList.remove("md:pl-24");
 			sidebar.classList.add("w-64");
 			sidebar.classList.remove("w-16");
+			avatar.classList.add("w-64");
+			avatar.classList.remove("w-16");
+			for (let item of items) {
+				item.classList.remove("hidden");
+			}
 		}
 	}
 }
